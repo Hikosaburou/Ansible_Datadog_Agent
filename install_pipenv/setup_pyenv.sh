9e9199588cd596f83123fa557539a6c611316fd9
EC2 Default User <ec2-user@ip-10-113-1-249.ap-northeast-1.compute.internal>
2019-08-28 09:35:20 +0000

    feat: ansible 実行環境 & Datadog(Ansible Galaxy)

--- install_pipenv/setup_pyenv.sh
+++ install_pipenv/setup_pyenv.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-set -xe
+set -x
 
 # Install pyenv
 echo 'Install Pyenv......'
@@ -10,13 +10,3 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
 exec "$SHELL" -l
-
-
-# Install other packages
-echo 'Install other packages......'
-sudo yum -y install gcc zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel
-
-
-# Install Pipenv
-echo 'Install Pipenv'
-sudo pip install pipenv
