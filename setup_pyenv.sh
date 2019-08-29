4759d04d54396c59aaef85c8bf5ebf59d8706c65
EC2 Default User <ec2-user@ip-10-113-1-249.ap-northeast-1.compute.internal>
2019-08-29 06:27:49 +0000

    feat: 整備

--- setup_pyenv.sh
+++ setup_pyenv.sh
@@ -9,4 +9,13 @@ git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
+
+# Install other packages
+echo 'Install other packages......'
+sudo yum -y install gcc zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel
+
+# Install Pipenv
+echo 'Install Pipenv'
+sudo `which pip` install pipenv
+
 exec "$SHELL" -l
